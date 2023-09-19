5a03b40b207e2dac0ffbeff180d500360c2bcc25
sarantium <47050317+sarantium@users.noreply.github.com>
2023-09-19 20:38:50 +1000

    enhancement: added sandbox

--- cheatsheet.md
+++ cheatsheet.md
@@ -14,3 +14,10 @@ https://github.com/mkdocs/mkdocs/issues/2184
 # langchain
 
 https://medium.com/badal-io/exploring-langchain-and-llamaindex-to-achieve-standardization-and-interoperability-in-large-2b5f3fabc360
+https://medium.com/@atef.ataya/building-summarization-apps-using-stuffdocumentschain-with-langchain-openai-6627c486424d
+
+# env
+
+https://sparkbyexamples.com/python/using-python-dotenv-load-environment-variables/
+https://pypi.org/project/python-dotenv/
+https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
---
+++ sandbox/summarise.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader, PyPDFLoader
+from langchain.chains.summarize import load_summarize_chain
+
+dotenv_path = Path(r"C:\Storage\python_projects\ashvin\.env")
+load_dotenv(dotenv_path=dotenv_path)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+loader = WebBaseLoader(
+    "https://medium.com/yeagerai/ai-agents-are-taking-us-from-software-to-autonomous-software-c1be5475a15b"
+)
+loader = WebBaseLoader(
+    "https://www.cloudflight.io/en/blog/the-architecture-for-an-entire-vehicle-twin-life/"
+)
+loader = PyPDFLoader(
+    "C:\Storage\python_projects\ashvin\docs\assets\deloitte_digital_twin.pdf"
+)
+docs = loader.load()
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+chain = load_summarize_chain(llm, chain_type="stuff")
+
+output = chain.run(docs)
+print(output)
