f01cb3e1edbc70d1acd6db0e28e8ee9add47cfee
sarantium <47050317+sarantium@users.noreply.github.com>
2023-09-23 21:15:12 +1000

    update: basics.py

--- sandbox/basics.py
+++ sandbox/basics.py
@@ -10,10 +10,10 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 # example one : basic direct use of llm and prompt
-from langchain.llms import OpenAI  # import llm
+# from langchain.llms import OpenAI  # import llm
 
-llm = OpenAI(temperature=0.7)  # instance llm
-prompt = "what is the population of India"  # prompt as string with no other inputs
+# llm = OpenAI(temperature=0.7)  # instance llm
+# prompt = "what is the population of India"  # prompt as string with no other inputs
 # predict = llm(prompt)  # output in variable
 
 # print(type(predict))  # output is string class
@@ -23,17 +23,36 @@ prompt = "what is the population of India"  # prompt as string with no other inp
 from langchain.prompts import PromptTemplate
 
 
-prompt_template = PromptTemplate.from_template(
-    "list the top five most {adjective} {items}"
-)
+# prompt_template = PromptTemplate.from_template(
+#     "list the top five most {adjective} {items}"
+# )
 
-prompt_template.format(adjective="livable", items="capital cities")
-full_prompt = prompt_template.format(adjective="livable", items="capital cities")
+# prompt_template.format(adjective="livable", items="capital cities")
+# full_prompt = prompt_template.format(adjective="livable", items="capital cities")
 
 # example three : use of basic LLMchain
 from langchain.chains import LLMChain
 
-chain = LLMChain(llm=llm, prompt=prompt_template)
-inputs = {"adjective": "livable", "items": "capital cities"}
-output = chain.run(inputs)
-print(output)
+# chain = LLMChain(llm=llm, prompt=prompt_template)
+# inputs = {"adjective": "livable", "items": "capital cities"}
+# output = chain.run(inputs)
+# print(output)
+
+# example four : use of basic LLM with a prompt
+# from langchain.llms import OpenAI
+
+# llm = OpenAI()
+# prompt = "Give me three reasons why digital twins are important"
+# output = llm(prompt)
+
+# print(output)
+
+# example five : use of basic LLM with a list of prompts
+from langchain.llms import OpenAI
+
+llm = OpenAI()
+prompt_list = ["tell me a joke", "tell me a lie"]
+output = llm.generate(prompts=prompt_list)
+print(type(output))
+print(output.generations[0][0])
+print(type(output.generations[0][0]))
--- sandbox/langchain.md
+++ sandbox/langchain.md
@@ -16,6 +16,8 @@
 
 Input :
 
+This is the modern, recommended input
+
 ```python
 from langchain.prompts import PromptTemplate
 
@@ -32,6 +34,19 @@ Output :
 "Tell me a funny joke about chickens."
 ```
 
+Input :
+
+This is an alternative, basic way of generating the Prompt Template and is seen in alot of the older tutorials available online. If you do it this way, then if the input variables don't match the template variables, it will raise an exception.
+
+```python
+from langchain.prompts import PromptTemplate
+
+prompt_template = PromptTemplate(
+    input_variables=["adjective"],
+    template="Tell me a {adjective} joke."
+)
+```
+
 ##### 1.1.1.2 Chat Prompt Template
 
 ![Alt text](/docs/assets/prompts_2.png)
@@ -40,3 +55,5 @@ Input
 ```python
 
 ```
+
+### 1.2 Language Models
