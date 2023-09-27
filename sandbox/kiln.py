b3b94b7ae0703ded5edc46dc7bfb1afddf7770b2
sarantium <47050317+sarantium@users.noreply.github.com>
2023-09-27 21:31:30 +1000

    enhancement: added kiln.py

---
+++ sandbox/kiln.py
@@ -0,0 +1,6 @@
+# main file to develop kiln agent app
+# all dev to be kept here until it gets too big to handle
+# TODOs
+# todo import env for OpenAI
+# todo import various libraries - Enum, ABC, dataclass to start with
+# todo from the start use docstrings
